29be6ea4f5e3b545a1f1fb7fcf27031e9006c299
MWaqasAfridi <153609017+MWaqasAfridi@users.noreply.github.com>
2024-03-15 07:44:15 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,138 @@
+// Question no 1
+var userName = [];
+
+
+// Question no 3
+// var stringArray=['waqas','sameer','nisar','tahir'];
+
+
+// Question no 4
+// var numberArray=[10,11,12,13,34,56];
+
+
+// Question no 5
+// var booleanArray=[true,false,true,true];
+
+
+// Question no 6 
+// var mixedArray=['waqas',12,true,'mustafa',20,];
+
+
+// Question no 7 (method 1)
+// document.write("<h1>Qualification</h1>")
+// var qualification=['SSC','HSC','BCS','BS','BCOM','MS','M.Phil','Phd'];
+// document.write("1) "+qualification[0]+"<br>");
+// document.write("2) "+qualification[1]+"<br>");
+// document.write("3) "+qualification[2]+"<br>");
+// document.write("4) "+qualification[3]+"<br>");
+// document.write("5) "+qualification[4]+"<br>");
+// document.write("6) "+qualification[5]+"<br>");
+// document.write("7) "+qualification[6]+"<br>");
+// document.write("8) "+qualification[7]+"<br>");
+
+
+// Question no 7 (method 2)
+// document.write("<h1>Qualification</h1>")
+// var qualification=['SSC','HSC','BCS','BS','BCOM','MS','M.Phil','Phd'];
+// document.write("<ol>");
+// document.write("<li>" + qualification[0] + "</li>");
+// document.write("<li>" + qualification[1] + "</li>");
+// document.write("<li>" + qualification[2] + "</li>");
+// document.write("<li>" + qualification[3] + "</li>");
+// document.write("<li>" + qualification[4] + "</li>");
+// document.write("<li>" + qualification[5] + "</li>");
+// document.write("<li>" + qualification[6] + "</li>");
+// document.write("<li>" + qualification[7] + "</li>");
+// document.write("</ol>");
+
+
+// //  Question no 8
+// //Array to store student names
+// var studentName=['waqas','sameer','tahir'];
+
+// //Array to store student scores
+// var studentScore=[483,277,162];
+
+// //Total marks for each student
+// var totalMarksEachStudent=500;
+
+// //Display scores and percentage for each student
+// var studentName1=studentName[0];
+// var studentScore1=studentScore[0];
+// var percentage=(studentScore1/500)*100;
+// console.log("Score of Waqas is: "+studentScore1+" and Percentage: "+percentage+"%");
+
+// var studentName2=studentName[1];
+// var studentScore2=studentScore[1];
+// var percentage=(studentScore2/500)*100;
+// console.log("Score of Sameer is: "+studentScore2+" and Percentage: "+percentage+"%");
+
+// var studentName3=studentName[2];
+// var studentScore3=studentScore[2];
+// var percentage=(studentScore3/500)*100;
+// console.log("Score of Tahir is: "+studentScore3+" and Percentage: "+percentage+"%");
+
+
+// Question no 9
+// var color = ['green','blue','white'];
+
+// var colorToAddAtBeginning = prompt("Enter a color to add to the beginning");
+// color.unshift(colorToAddAtBeginning);
+
+// var colorToAddAtEnd = prompt("Enter a color to add to the End");
+// color.push(colorToAddAtEnd);
+
+// color.unshift('orange','red');
+
+// color.shift();
+
+// color.pop();
+
+// var indexToAddColor = prompt("Enter your index where you want to add a color");
+// var colorToAddAtIndex = prompt("Enter your color you want to add");
+// color.splice(indexToAddColor,0,colorToAddAtIndex)
+
+// var indexToDeltColor = prompt("Enter your index from where you want to delt a color");
+// var numberOfColorsToDelt = prompt("Enter how many colors you want to delt");
+
+// color.splice(indexToDeltColor,numberOfColorsToDelt);
+// document.write(color);
+
+
+// Question no 10
+// var studentScore=[320,230,480,120];
+// console.log("Scores of Students: "+studentScore);
+// studentScore.sort();
+// console.log("Ordered Scores of Students: "+studentScore);
+
+
+// Question no 11
+// var cities=['Karachi','Lahore','Islamabad','Quetta','Peshawar'];
+// console.log("Cities list: "+cities);
+// var selectedcities=cities.slice(2,4);
+// console.log("Selected Cities list: "+selectedcities);
+
+
+// Quesion no 12
+// var arr=["This","is","my","cat"];
+// console.log("Array: "+arr);
+// var singleString=arr.join(" ");
+// console.log("String: "+singleString);
+
+
+// Question no 15
+// var phoneManufactures = ["Apple","Samsung","Motorola","Nokia","Sony","Haier"];
+
+// document.write("<select>")
+// document.write("<option>" + phoneManufactures[0] + "</option>")
+// document.write("<option>" + phoneManufactures[1] + "</option>")
+// document.write("<option>" + phoneManufactures[2] + "</option>")
+// document.write("<option>" + phoneManufactures[3] + "</option>")
+// document.write("<option>" + phoneManufactures[4] + "</option>")
+// document.write("<option>" + phoneManufactures[5] + "</option>")
+// document.write("</select>")
+
+
+
+
+
